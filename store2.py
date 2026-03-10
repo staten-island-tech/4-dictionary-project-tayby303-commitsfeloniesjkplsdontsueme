b415ded0c2b5a5af7d8e3321179469c2bc270684
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-03-10 10:27:13 -0400

    commit

--- store2.py
+++ store2.py
@@ -8,12 +8,14 @@ stock=[
 for index, item in enumerate(stock):
     print(index, ":", item["name"]) #prints list of items & their index inside stock
 cart=[]  #list of items you buy
-def 
+def machine():
     item=int(input("Please enter the index number of the item of want to purchase: "))  #asks the items from stock you want
     cart.append(stock[item])    #adds the item you input into cart
     cashier=input("Are you done shopping yet? yes/no ")  #asks if you are done shopping
     register=cashier.lower()    #makes the answer for cashier lowercase
+    return register
 while register()=="no":
+    machine()
     item=int(input("Please enter the index number of the item of want to purchase: "))  #as long as regiser says no, you will be asked to input another item from stock
 if register()=="yes":
     for index, item in enumerate(cart):
