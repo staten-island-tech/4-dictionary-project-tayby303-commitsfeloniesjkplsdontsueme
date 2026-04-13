4585514a8dff39b2047cbe23f04b0738bd478529
tayby303-commitsfeloniesjkplsdontsueme <tayby303@gmail.com>
2026-04-13 09:49:59 -0400

    commit

--- store2.py
+++ store2.py
@@ -2,7 +2,7 @@ stock=[{"name":"hot dog combo", "price": 1.50, "desc": "a hot dog and a drink"},
 {"name":"80in TV","price":999.99,"desc":"a 4K UHD 80in Samsung TV"},
 {"name":"LEGO Nissan Skyline","price":72.95,"desc":"Miniature Nissan Skyline made out of LEGO"}]   #items in stock
 for index, item in enumerate(stock):
-    print(index, ":", item["name"]) #prints list of items & their index inside stock
+    print(index, ":", item["name"], "; $", item["price"], ";", item["desc"]) #prints list of items & their index inside stock
 cart=[]  #list of items you buy
 def store(cart):
     item=int(input("Please enter the index number of the item you want to purchase: "))  #asks the items from stock you want
